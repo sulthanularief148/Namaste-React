4fea1b28fe2f6a3602e16441338761f4b573f8c6
webfarmtech <webfarmtechfreelancers@gmail.com>
2025-01-15 14:54:02 +0530

    userclass removed

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,4 @@
 import { Component } from "react";
-import UserClass from "./UserClass";
-import UserContext from "../utils/UserContext";
-
 class About extends Component {
   constructor() {
     super();
@@ -15,23 +12,23 @@ class About extends Component {
       },
     };
   }
-  async componentDidMount() {
-    const data = await fetch("https://api.github.com/users/ariefsulthanul468");
-    const response = await data.json();
-    this.setState({ userInfo: response });
-  }
+  // async componentDidMount() {
+  //   const data = await fetch("https://api.github.com/users/ariefsulthanul468");
+  //   const response = await data.json();
+  //   this.setState({ userInfo: response });
+  // }
   render() {
     const userData = this.state.userInfo;
     return (
       <>
 
-          <UserClass
-            image={userData?.avatar_url}
-            email={userData?.email || "ariefsulthanul468@gmail.com"}
-            mobile={userData.mobile || "9659051798"}
-            location={userData.location || "Portonovo"}
-          />
-     
+        <UserClass
+          image={userData?.avatar_url}
+          email={userData?.email || "ariefsulthanul468@gmail.com"}
+          mobile={userData.mobile || "9659051798"}
+          location={userData.location || "Portonovo"}
+        />
+
       </>
     );
   }
