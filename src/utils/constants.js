a4c2f8df2aaab266ffe9b2e0c4a0067c7076edfd
webfarmtech <webfarmtechfreelancers@gmail.com>
2025-02-14 19:20:10 +0530

    menu url removed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660";
 
-export default MENU_API_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0843007&lng=80.2704622&restaurantId=";
+//export default MENU_API_URL ="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0843007&lng=80.2704622&restaurantId=";
